de5ca8ae3771b97cb90ea18166462716e2261923
harol.guzman <harol.stiven147@gmail.com>
2021-09-17 11:15:47 -0500

    Tabla v. Alpha

--- app/src/main/java/com/example/firebase2/reportesActivity.java
+++ app/src/main/java/com/example/firebase2/reportesActivity.java
@@ -3,13 +3,30 @@ package com.example.firebase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class reportesActivity extends AppCompatActivity {
+
+    private TableLayout tableLayout;
+    private TableDynamic tableDynamic;
+    private String[] encabezado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
+
+        tableLayout = findViewById(R.id.tableReportes);
+        tableDynamic = new TableDynamic(tableLayout, getApplicationContext());
+        encabezado = new String[]{"Documento", "Nombre"};
+
+        try {
+            tableLayout.removeAllViews();
+            tableDynamic.addHeader(encabezado);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     /*
