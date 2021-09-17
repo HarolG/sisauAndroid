e2407dd61c0a9daa42dee06057208fdbaeb0ca67
Harol <harol.stiven147@gmail.com>
2021-09-17 09:54:09 -0500

    comentarios guia para la tabla

--- app/src/main/java/com/example/firebase2/reportesActivity.java
+++ app/src/main/java/com/example/firebase2/reportesActivity.java
@@ -11,4 +11,19 @@ public class reportesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
     }
+
+    /*
+        private TableLayout tableRegister;
+        TableDynamic tableDynamic = new TableDynamic(tableRegister, getApplicationContext());
+
+         try {
+                tableRegister.setVisibility(View.VISIBLE);
+                //getRegistrosBd("https://crocodilian-quart.000webhostapp.com/api/user.php");
+                tableRegister.removeAllViews();
+                tableDynamic.addHeader(headers);
+                tableDynamic.addData(getRegistrosTabla("https://crocodilian-quart.000webhostapp.com/api/user.php"));
+             } catch (Exception e) {
+                   Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+               }
+     */
 }
